5b08ceb9571da955faf289763369ac1d98824cf6
jnicholson-wcgs <54621945+jnicholson-wcgs@users.noreply.github.com>
2022-02-03 15:19:22 +0000

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,30 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+    
+    
+    // Task 1: Print out two strings which have been concatenated (joined) together
+    
+    /*
+     * Task 2: Prompt the user for an integer value
+     *         Output whether the integer is a) greater that 10, b) equal to 10 or c) less than 10
+     */
+     
+     // Task 3: Define a constant float called PI.
+     //         Prompt the user for a radius, and using PI calcuate and print the circumference and area
+     
+     /* Task 4 */
+     /* 
+      * Prompt the user for an integer. 
+      * Using the MOD operator, print out if it is odd or even
+      */
+      
+      // Task 5
+      // Prompt the user for a string
+      // a) Determine whether the string contains the substring "ab"
+      // b) Print out the last 3 characters of the string
+      
+  }
+}
